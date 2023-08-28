7d9434cdaf283b21fd3c19e12fadae9735fea9a7
Jose Cuevas <josecueram@gmail.com>
2023-08-28 16:16:47 -0600

    Add GitHub logo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import { Inconsolata } from "next/font/google";
+import { FaGithubSquare } from "react-icons/fa";
 
 const inconsolata = Inconsolata({ subsets: ["latin"] });
 
@@ -9,6 +10,14 @@ export default function Home() {
     >
       <h1 className="text-6xl text-blue-800">José Cuevas</h1>
       <p>I&apos;ll put something here eventually.</p>
+      <a
+        href="https://github.com/JoseCrz"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="mt-12 text-2xl hover:text-blue-900"
+      >
+        <FaGithubSquare />
+      </a>
     </main>
   );
 }
