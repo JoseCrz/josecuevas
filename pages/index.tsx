cddf354387cbadd3383b4f0969922c8d18b62431
Jose Cuevas <josecueram@gmail.com>
2023-08-28 16:07:25 -0600

    Add placeholder content

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,10 @@ const inconsolata = Inconsolata({ subsets: ["latin"] });
 export default function Home() {
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inconsolata.className}`}
-    ></main>
+      className={`flex min-h-screen flex-col items-center justify-center sm:p-6 md:p-24 ${inconsolata.className}`}
+    >
+      <h1 className="text-6xl text-blue-800">José Cuevas</h1>
+      <p>I&apos;ll put something here eventually.</p>
+    </main>
   );
 }
